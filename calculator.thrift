b2469999fb44e869bc23c1b49491a36d051c4a88
Jun Gao <0618johnny@gmail.com>
2023-03-04 09:34:36 -0800

    Create calculator.thrift

---
+++ calculator.thrift
@@ -0,0 +1,11 @@
+# This IDL file defines a Thrift service named "Calculator" with four methods: add, subtract, multiply, and divide. 
+# The methods take two arguments of type i32 or double, and return an i32 or double value. The divide method also 
+# throws an exception of type string if the second argument is zero.
+
+namespace tutorial
+service Calculator {
+    i32 add(1: i32 num1, 2: i32 num2),
+    i32 subtract(1: i32 num1, 2: i32 num2),
+    i32 multiply(1: i32 num1, 2: i32 num2),
+    double divide(1: double num1, 2: double num2) throws (1: string message)
+}
